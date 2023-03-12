4ca9f091ba908c4a1a51b6f3a59eb23d22c11182
NARESH CHOUDHARY <nareshnc82643@gmail.com>
2023-03-12 18:37:29 +0530

    added deploy and verify scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,52 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKET_API_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-    solidity: "^0.8.8",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.9" }, { version: "0.6.6" }],
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+    // gasReporter: {
+    //     enabled: REPORT_GAS,
+    //     currency: "USD",
+    //     outputFile: "gas-report.txt",
+    //     noColors: true,
+    //     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    // },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+        },
+        player: {
+            default: 1, // similarly on mainnet it will take the first account as deployer.
+        }, //Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
 };
